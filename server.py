fe6d3b4cfbe305575151ad72750ec8691576099d
Vlad <vsakayafignavlad@gmail.com>
2021-11-06 15:21:24 +0300

    bind on server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from socket import *
 server = ('89.108.79.2', 50666)
 sock = socket(AF_INET, SOCK_DGRAM)
+sock.bind(server)
 def exchange(sock):
     data1, addr1 = sock.recvfrom(1024)
     data2, addr2 = sock.recvfrom(1024)
