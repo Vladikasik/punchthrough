6dcd71dbf9f07381ba9f5a0298ebb41de40dba3f
Vlad <vsakayafignavlad@gmail.com>
2021-11-06 17:42:46 +0300

    mulitprocessing

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 from socket import *
-import threading as th
+import multiprocessing as mp
 priv = input('your private ip>> '), int(input('your private port>> '))
 server = ('89.108.79.2', 50666)
 
@@ -15,7 +15,7 @@ resp = resp.decode('utf-8')
 print(f'got data from server -> "{resp}"')
 
 def call_other(addr, sock):
-    for _ in range(5):
+    for _ in range(10):
         print(f'"test" -> {addr}')
         sock.sendto(b'test1', addr)
         
@@ -23,6 +23,7 @@ def listen(sock):
     resp, addr = sock.recvfrom(1024)
     resp = resp.decode('utf-8')
     print(f'{resp} <- "{addr}"')
+    return 0
     
 public = resp.split('$')[0].split('#')
 pb_addr = (public[0], int(public[1]))
@@ -32,14 +33,14 @@ private = resp.split('$')[1].split('#')
 pr_addr = (private[0], int(private[1]))
 print('another node pr_addr', pr_addr)
 
-pub = th.Thread(target=call_other, args=[pb_addr, sock])
-priv = th.Thread(target=call_other, args=[pr_addr, sock])
-lstn = th.Thread(target=listen, args=[sock])
+pub = mp.Process(target=call_other, args=(pb_addr, sock,))
+priv = mp.Process(target=call_other, args=(pr_addr, sock,))
+lstn = mp.Process(target=listen, args=(sock,))
 lstn.start()
 pub.start()
 priv.start()
 
 lstn.join() # дождедтся окончания листена
 
-pub.kill()
-priv.kill()
+pub.terminate()
+priv.terminate()
