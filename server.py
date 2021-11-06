09264a09fdc74f76e4a98de5a3422bd223523f4e
Vlad <vsakayafignavlad@gmail.com>
2021-11-06 15:27:15 +0300

    a bit more encoding

--- server.py
+++ server.py
@@ -11,8 +11,8 @@ def exchange(sock):
     data2 = data2.decode('utf-8')
     print(f'"{data2}" <- {addr2}')
 
-    data1 = data1 + '$' + addr1[0] + addr1[1]
-    data2 = data2 + '$' + addr2[0] + addr2[1]
+    data1 = data1 + '$' + str(addr1[0]) + str(addr1[1])
+    data2 = data2 + '$' + str(addr2[0]) + str(addr2[1])
     
     sock.sendto(data1, addr2)
     sock.sendto(data2, addr1)
